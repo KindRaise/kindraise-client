75325cbd9faadf3913c4ca443483a63c126f4219
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-09-10 16:10:32 +0100

    Build successful and changes committed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import './Hero.css'
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import LoveImage from '../../assets/love-image.png';
+import './Hero.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Hero
+  useEffect(() => {
+    AOS.init({
+      duration: 3000,
+      once: false,
+      mirror: true,
+    });
+  }, []);
+
+  return (
+    <section className="hero-section">
+      <div className="hero-content-wrapper">
+        <div className="image-wrapper" data-aos="fade-right">
+          <div className="image-container">
+            <img src={LoveImage} alt="Fundraising" className="hero-image" />
+          </div>
+        </div>
+
+        <div className="text-wrapper" data-aos="fade-left">
+          <h1>Do something <br />incredible today</h1>
+          <p>
+            Empower the causes you care <br />about. Fundraise with ease.
+          </p>
+
+          <button onClick={() => navigate('/signup')} className="cta-button">Get Started</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
