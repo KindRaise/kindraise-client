bbcf7ca745404a4666139f1c87af40160a65465f
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-09-17 22:04:56 +0100

    test run

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import './ContactUs.css'
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <>
+    <Header/>
+    <div className='contact-container'>
+        <h1>Get in touch</h1>
+        <p>We are happy to help. Please contact us if you need help or have any questions.</p>
+
+        <div className='container-inner'>
+
+        </div>
+
+    </div>
+    <Footer/>
+    </>
   )
 }
 
